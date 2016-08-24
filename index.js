d54a9c475460eaeb978678703e8adcd41da75440
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-24 19:31:03 -0400

    bar charts moved into div for each branch

--- index.js
+++ index.js
@@ -16,43 +16,9 @@ function encodeBranchPercent(branch) {
   // console.log(branchPercents);
 }
 
-// function barChart(yearlyRevenues) {
-//   var svg1 = d3.select('.svg-1');
-//   var rects = svg1.selectAll('rect').data(yearlyRevenues);
-//   var newRects = rects.enter();
-
-//   var maxCount = d3.max(yearlyRevenues, function(d, i) {
-//     return parseInt(d.yearly_forecast);
-//   });
-
-//   console.log(d3);
-
-//   var x = d3.scaleLinear()
-//     .range([0, 500])
-//     .domain([0, maxCount]);
-//   var y = d3.scaleBand()
-//     // .rangeRound([0, 50])
-//     .domain(yearlyRevenues.map(function(d,i) {
-//       return d.branch_name;
-//     }));
-//   // console.log("max is", maxCount);
-
-//   newRects.append('rect')
-//     .attr('x', x(0))
-//     .attr('y', function(d,i) {
-//       return d.branch_name;
-//     })
-//     .attr('height', 25)
-//     .attr('width', function(d,i) {
-//       return x(d.yearly_forecast);
-//     })
-//     .attr('fill', 'red');
-// }
-
-document.addEventListener('DOMContentLoaded', function() {
-
-  // barChart(yearlyRevenues);
-  var svg = d3.select('body')
+function cbFunction(ele, ind, yearlyRevenues) {
+  console.log("element:", ele);
+  var svg = d3.select('#' + ele.branch_name.replace(" ", "-")) // remove space and add dash to match id from div
     .append('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
@@ -84,6 +50,44 @@ document.addEventListener('DOMContentLoaded', function() {
       return i * (svgWidth / yearlyRevenues.length);
     })
     .attr('y', svgHeight);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  yearlyRevenues.forEach(cbFunction);
+
+  // var svg = d3.select('.svg-1')
+  //   .append('svg')
+  //   .attr('width', svgWidth)
+  //   .attr('height', svgHeight);
+
+  // svg.selectAll('rect')
+  //   .data(yearlyRevenues)
+  //   .enter()
+  //   .append('rect')
+  //   .attr('x', function(d, i) {
+  //     return i * (svgWidth / yearlyRevenues.length); // width of svg divided by how many elements in the array multiplied by index of element
+  //   })
+  //   .attr('y', function(d, i) {
+  //     return svgHeight - d.yearly_forecast / 100000; // because svg y-axis begins from top, we must offset each bar towards the bottom
+  //   })
+  //   .attr('width', svgWidth / yearlyRevenues.length - barPadding)
+  //   .attr('height', function(d, i) {
+  //     return d.yearly_forecast / 100000; // scale down to fit bars in <svg>
+  //   })
+  //   .attr('fill', 'red');
+
+  // svg.selectAll('text')
+  //   .data(yearlyRevenues)
+  //   .enter()
+  //   .append('text')
+  //   .text(function(d, i) {
+  //     return d.branch_name;
+  //   })
+  //   .attr('x', function(d, i) {
+  //     return i * (svgWidth / yearlyRevenues.length);
+  //   })
+  //   .attr('y', svgHeight);
 
 
 
